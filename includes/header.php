3d09477550203e69adef4340d5f93f1c0dea6514
roddom67 <roddom67@gmail.com>
2018-11-10 18:34:13 -0300

    tag manager

--- includes/header.php
+++ includes/header.php
@@ -22,10 +22,21 @@
 	<meta property="og:site_name" content="Fotografía de Rod"/>
 	<meta property="og:description" content="<?php echo $desc[$n];?>" />
 	<meta property="og:image" content="http://fotografiaderod.com.ar/imgs/logo_Rod.fw.png" />
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W9KVKXQ');</script>
+<!-- End Google Tag Manager -->
 </head>
 
 <body>
 <?php include_once("analyticstracking.php") ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9KVKXQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="contenedor">
 	<header>
 		<?php include 'botonera.php'; ?>
